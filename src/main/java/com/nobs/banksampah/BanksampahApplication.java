1122b71035de155e22223d017708c5c3ac6a5f5a
arycenas <672021189@student.uksw.edu>
2024-06-21 17:36:20 +0700

    feat: application.properties, folder controller, model, repository

--- src/main/java/com/nobs/banksampah/BanksampahApplication.java
+++ src/main/java/com/nobs/banksampah/BanksampahApplication.java
@@ -8,15 +8,6 @@ public class BanksampahApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanksampahApplication.class, args);
-		System.out.println("Hello Lila");
-		System.out.println("Halo Akbar");
-		System.out.println("Halo Rafa");
-		System.out.println("coba cobba");
-		System.out.println("Testing");
-		System.out.println("ll");
-		System.out.println("Haha");
-		System.out.println("rafael daendels");
-		System.out.println("daendels animalia");
 	}
 
 }
